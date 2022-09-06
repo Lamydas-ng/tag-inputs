65b690a0ed808c67a05224bc24757f88274bc2d4
Lamydas-ng <84775623+Lamydas-ng@users.noreply.github.com>
2022-09-06 20:53:04 +0100

    Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: () => ({
+    jsFrameworks: ["react.js", "vue.js", "angular"],
+    backendFrameworks: [],
+  }),
+  methods: {
+    handleChange(tags) {
+      this.jsFrameworks = [...tags];
+    },
+  },
+};
 </script>
+
+<template>
+  <h1>Your favorite frameworks</h1>
+  <div>{{ jsFrameworks }}</div>
+  <tags-input :selected-tags="jsFrameworks" @change="handleChange" />
+
+  <div>{{ backendFrameworks }}</div>
+  <tags-input
+    :selected-tags="backendFrameworks"
+    @change="backendFrameworks = [...$event]"
+  />
+</template>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  color: blue;
 }
 </style>
